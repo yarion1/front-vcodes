a9b856f6703ecd90791bef1bdcfee86526b93d03
Pablo Henrique <74999976+yarion1@users.noreply.github.com>
2022-06-11 16:24:34 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,17 @@
 import React from "react";
-import { HashRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Login from "./pages/login";
 import Home from "./pages/home/index";
 import PrivateRoute from "./services/wAuth";
 
 function Rotas() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Login} />
         <PrivateRoute path="/home" exact component={Home} />
       </Switch>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 export default Rotas;
